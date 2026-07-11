0154b137223d9cdbb938aa617131c6608df877ac
SangNT31 <SangNT31@users.noreply.github.com>
2026-07-11 13:52:26 +0700

    fix mobile album layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "v26";
+const VERSION = "v27";
 const STATIC_CACHE = `baby-album-static-${VERSION}`;
 const IMAGE_CACHE = `baby-album-images-${VERSION}`;
 const MAX_CACHED_IMAGES = 60;
